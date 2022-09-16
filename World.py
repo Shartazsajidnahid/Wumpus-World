5a8378916b46abea1303b92aff585d9b9b047cdb
Shartazsajidnahid <sajidnahid9658@gmail.com>
2022-09-16 12:59:20 +0600

    helpers of addfeature added, addfeature method yet to implement

--- World.py
+++ World.py
@@ -30,3 +30,37 @@ class World():
         # set the AI function 
             
         # set the game board
+
+    # def __addFeatures ( self, file = None ):
+
+    def __addPit ( self, c, r ):
+        if self.__isInBounds(c, r):
+            self.__board[c][r].pit = True
+            self.__addBreeze ( c+1, r )
+            self.__addBreeze ( c-1, r )
+            self.__addBreeze ( c, r+1 )
+            self.__addBreeze ( c, r-1 )
+
+    def __addWumpus ( self, c, r ):
+        if self.__isInBounds(c, r):
+            self.__board[c][r].wumpus = True
+            self.__addStench ( c+1, r )
+            self.__addStench ( c-1, r )
+            self.__addStench ( c, r+1 )
+            self.__addStench ( c, r-1 )
+    
+    def __addGold ( self, c, r ):
+        if self.__isInBounds(c, r):
+            self.__board[c][r].gold = True
+    
+    def __addStench ( self, c, r ):
+        if self.__isInBounds(c, r):
+            self.__board[c][r].stench = True
+    
+    def __addBreeze ( self, c, r ):
+        if self.__isInBounds(c, r):
+            self.__board[c][r].breeze = True
+    
+    def __isInBounds ( self, c, r ):
+        return c < self.__colDimension and r < self.__rowDimension and c >= 0 and r >= 0
+    
